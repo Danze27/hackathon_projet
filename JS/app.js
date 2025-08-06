d2ee2a05a41d985f58955df045cb0a141ae81ae1
0shirase <lucas0999.lb@gmail.com>
2025-08-06 10:03:10 +0200

    FEAT: création du backend pour le register/login

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,34 @@
 // TODO Module
-const express = require('express')
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+const express = require("express");
 const app = express();
-const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/authRoutes");
 
 // TODO Fichiers
-const connectMongoDB = require('./data/mongoData');
+const connectMongoDB = require("./data/mongoData");
 
 // TODO Coniguration ejs
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/../views')
-app.set('layout', 'layout')
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/../views");
+app.set("layout", "layout");
 
 // TODO Middleware
 app.use(express.json());
 app.use(expressLayouts);
-
+app.use(cookieParser());
 
 // TODO Connexion à MongoDB
-connectMongoDB() 
+connectMongoDB();
 
-app.use('/', (req, res) => {
-    console.log('Request received');
-})
+app.use("/", authRoutes);
+app.use("/", (req, res, next) => {
+  console.log("Request received");
+  next();
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
